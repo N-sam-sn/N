4bc6aec58f3703dc84140f5239432c64228423d1
N-sam-sn <snsamsn4@gmail.com>
2025-05-30 15:52:26 +0300

    Update dash02.py

--- dash02.py
+++ dash02.py
@@ -8,7 +8,7 @@ from io import BytesIO
 # Загружаем и отображаем эмодзи
 #emoji_image = Image.open("B01r.png")
 #st.image(emoji_image, width=80)
-EMOJI_URL = "https://cdn.jsdelivr.net/gh/N-sam-sn/N@main/B01r.png" # EMOJI_URL ="https://github.com/N-sam-sn/N/blob/main/dash02.py" #"https://raw.githubusercontent.com/N-sam-sn/N/main/B01r.png"
+EMOJI_URL = "https://raw.githubusercontent.com/N-sam-sn/N/main/B01r.png" # EMOJI_URL ="https://github.com/N-sam-sn/N/blob/main/dash02.py" #"https://raw.githubusercontent.com/N-sam-sn/N/main/B01r.png"
 
 try:
     response = requests.get(EMOJI_URL, timeout=5)
